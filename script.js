c0508cd9c6e5149d380cd6eabb85857109e5f5f6
Shade <anhvu_2027@depauw.edu>
2024-11-15 17:39:36 -0500

    Split pieces into black and white

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const PLAYER_DISPLAY = document.querySelector("#player");
 const INFO_DISPLAY = document.querySelector("#info-display");
 const WIDTH = 8;
 
-const START_PIECES = [
+const START_PIECES = [ ///Initialization need rework
     ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK,
     PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN, PAWN,
     '', '', '', '', '', '', '', '',
@@ -19,17 +19,27 @@ function createBoard() {
         let square = document.createElement('div');
         square.classList.add('square');
         square.innerHTML = startPiece;
+        
         square.setAttribute('square-id', i);
 
         let row = Math.floor((63 - i) / 8) + 1;
         if (row % 2 == 0) {
-            square.classList.add(i % 2 == 0 ? "white" : "black");
+            square.classList.add(i % 2 == 0 ? 'white' : 'black');
         }
         else {
-            square.classList.add(i % 2 == 0 ? "black" : "white");
+            square.classList.add(i % 2 == 0 ? 'black' : 'white');
         }
+
+        if (i <= 15) { ///Logic need rework
+            square.firstChild.firstChild.classList.add('blackPiece');
+        }  
+
+        if (i >= 48) { ///Logic need rework
+            square.firstChild.firstChild.classList.add('whitePiece');
+        }  
 
         GAME_BOARD.append(square);
     })
 }
+
 createBoard();
